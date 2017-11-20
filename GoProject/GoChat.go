ca5e6fbc40747696feef33fbd8fa5ff0584ebcd5
badwulf51 <aronomalley@yahoo.com>
2017-11-20 17:47:52 +0000

    Update GoChat.go
    Added more conversation between user and bot, tried to capture "she was" but could not get it to work, feels like work is going no where...

--- GoProject/GoChat.go
+++ GoProject/GoChat.go
@@ -26,12 +26,35 @@ func ElizaResponse(inputStr string) string{
 		//return the string below
 		return "Why dont you tell me more about your mother?"
 	}
+	
+	
+	if matched, _ := regexp.MatchString(`(?i).*\bschool\b.*`, input); matched {
+		//return the string below
+		return "Blah! I hate school! Talk about anything else, please"
+	}
 
 //capture I am
-	re := regexp.MustCompile("I am ([^.!?]*)[.!?]?")
+	re := regexp.MustCompile("[iI] am ([^.!?]*)[.!?]?")
 	
 	if re.MatchString(input){
 		return re.ReplaceAllString(input, "How do you know you are $1?") 
+
+	}
+	
+// Capture He was 
+	re = regexp.MustCompile("[hH]e was ([^.!?]*)[.!?]?")
+	
+	if re.MatchString(input){
+		return re.ReplaceAllString(input, "Are you SURE he was $1?") 
+
+	}
+	
+	// Capture She was
+	// xir is temporary, for some reason i cant get it to capture she just for the time being 
+	re = regexp.MustCompile("[xX]ir was ([^.!?]*)[.!?]?")
+	
+	if re.MatchString(input){
+		return re.ReplaceAllString(input, "Are you SURE xir was $1?") 
 
 	}
 
@@ -45,6 +68,8 @@ func ElizaResponse(inputStr string) string{
 		 "Phuhuhuhu! You're very funny!",
 		 "Thats a topic I dont even wanna touch!",
 		 "hmmmmm...explain in detail?",
+		 "Do you kiss your mother with that mouth?!",
+		 "Gee thats something you dont want your father to hear!",
 		 
 		 
 	}
@@ -86,9 +111,8 @@ func main(){
 	rand.Seed(time.Now().UTC().UnixNano())//get a random number
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Please message me")
-	// Keeps the conversation looping 
 	i := 1
-	for i <= 1000 {
+	for i <= 5 {
 		i++
 	input, _ := reader.ReadString('\n')
 	fmt.Scanf("%s", &input)
